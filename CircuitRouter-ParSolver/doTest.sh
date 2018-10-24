f8d7ba842f9c9a641e254d3841268e0d46ac83f7
Beatriz Martins <mariabeatriz.dongamartins@gmail.com>
2018-10-24 12:37:58 +0100

    bug with output file name, the rest is done

--- CircuitRouter-ParSolver/doTest.sh
+++ CircuitRouter-ParSolver/doTest.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-NUMTASKS=$1
-FILEPATH=$2
+OUTPUT_FILE="$FILEPATH$A"
 
-echo $1 $2
+COUNTER=1
+NUM_TASKS=$1
+FILE_PATH=$2
 
-SEQSOLVERPATH='../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver'
-PARSOLVERPATH='../CircuitRouter-ParSolver/CircuitRouter-ParSolver'
-A='.speedup.csv'
+SEQSOLVER_PATH='../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver'
+PARSOLVER_PATH='../CircuitRouter-ParSolver/CircuitRouter-ParSolver'
 
-$($SEQSOLVERPATH $FILEPATH)
+RES_ADD='.res'
+OUTPUT_ADD='.speedup.csv'
+
+RES_FILE="$FILE_PATH$RES_ADD"
+
+($SEQSOLVER_PATH $FILE_PATH)
+SEQSOLVER_TIME=$(grep 'Elapsed time' $RES_FILE | cut -b 19-26)
+
+echo '#threads,exec_time,speedup' > "{$OUTPUT_FILE}"
+echo "1S,$SEQSOLVER_TIME,1" >> "{$FILEPATH$A}"
+
+#($PARSOLVER_PATH -t $NUM_TASKS $FILE_PATH)
+#PARSOLVER_TIME=$(grep 'Elapsed time' $RES_FILE | cut -b 19-26)
+#speedup=$(echo "scale=6; ${SEQSOLVER_TIME}/${PARSOLVER_TIME}" | bc)
+echo '1,PARSOLVER_TIME,speedup' >> "{$FILEPATH$A}"
+
+while [ $COUNTER -lt $NUM_TASKS ]; do
+    let COUNTER=COUNTER+1
+    ($SEQSOLVER_PATH $FILE_PATH)
+    SEQSOLVER_TIME=$(grep 'Elapsed time' $RES_FILE | cut -b 19-26)
+    #($PARSOLVER_PATH -t $NUM_TASKS $FILE_PATH)
+    #PARSOLVER_TIME=$(grep 'Elapsed time' $RES_FILE | cut -b 19-26)
+    #speedup=$(echo "scale=6; ${SEQSOLVER_TIME}/${PARSOLVER_TIME}" | bc)
+    echo "$COUNTER,PARSOLVERTIME,speedup" >> "{$FILEPATH$A}"
+
+done
 
 
-#($PARSOLVERPATH -t $NUMTASKS $FILEPATH)
-
-FILE="$FILEPATH$A"
-echo $FILE
